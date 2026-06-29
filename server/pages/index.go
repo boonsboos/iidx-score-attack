c620cc01f142593c4fbf65c52b750165af9c57e1
boonsboos <boons@boonsboos.nl>
2026-06-29 22:33:27 +0200

    fix: show when there is no active bracket

--- server/pages/index.go
+++ server/pages/index.go
@@ -1,9 +1,12 @@
 package pages
 
 import (
+	"errors"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"iidx.boonsboos.nl/server/config"
 	"iidx.boonsboos.nl/server/db"
 	"iidx.boonsboos.nl/server/models"
@@ -15,15 +18,24 @@ func Index(context *gin.Context) {
 	lowerBracketCharts := make([]models.FrontendBracketChart, 0)
 
 	activeChartPool, err := db.GetCurrentlyActiveChartPool()
-	if err == nil {
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Println("No active chart pool found")
+		} else {
+			log.Println("Error fetching active chart pool:", err)
+		}
+	} else {
 		upper, lower, err := db.GetPoolChartsFrontend(activeChartPool)
-		if err == nil {
+		if err != nil {
+			log.Println("Error fetching active charts for frontend:", err)
+		} else {
 			upperBracketCharts = upper
 			lowerBracketCharts = lower
 		}
 	}
 
 	context.HTML(http.StatusOK, "index.html", gin.H{
+		"BracketActive":      activeChartPool.ID != 0,
 		"ClientId":           config.ServerConfig.OauthClientId,
 		"RedirectURI":        config.ServerConfig.OauthRedirectUrl,
 		"PoolName":           activeChartPool.Title,
