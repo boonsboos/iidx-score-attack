0e8e51c53c4f4fff85ab3d58623f5c68a86f51f0
boonsboos <boons@boonsboos.nl>
2026-07-05 03:06:04 +0200

    fix: use regular gorm api to update specific columns only

--- server/job/scorejob.go
+++ server/job/scorejob.go
@@ -127,15 +127,11 @@ func playerJob(player models.Player, activeBracketCharts []models.BracketChart) 
 	// update the profile if it has changed
 	if profile.SPDanLevel > player.DanLevel || profile.DJName != player.DJName {
 		log.Println("Updating player", player.GameID, "with new profile data")
-		_, err := gorm.G[models.Player](db.DB).
-			Where("id = ?", player.ID).
-			Omit("refresh_token").
-			Updates(db.DefaultTimeout(), models.Player{
-				DJName: profile.DJName,
-				// churns, but the BeforeUpdate hook requires the refresh token to be set
-				RefreshToken: player.RefreshToken,
-				DanLevel:     profile.SPDanLevel,
-			})
+
+		err := db.DB.Model(&player).UpdateColumns(models.Player{
+			DJName:   profile.DJName,
+			DanLevel: profile.SPDanLevel,
+		}).Error
 		if err != nil {
 			// TODO: notify maintainer
 			log.Println("Error occurred while updating player profile for player", player.GameID, ":", err)
